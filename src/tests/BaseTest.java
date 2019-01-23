2aecbafeb84668b7d758d1e4982ae9c5835c495a
gabrielagarayzavalia <36867961+gabrielagarayzavalia@users.noreply.github.com>
2019-01-23 08:35:26 -0300

    Update BaseTest.java

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -1,11 +1,12 @@
 package tests;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 
 public class BaseTest {
 
